0c15a2147049be50ce9b3108b1cd17fae3725065
Nilesh <nilesh.nilesh@sap.com>
2025-07-01 14:44:01 +0530

    feat: CORS

--- src/main/java/com/skillmesh/backend/config/GlobalCorsConfig.java
+++ src/main/java/com/skillmesh/backend/config/GlobalCorsConfig.java
@@ -9,7 +9,7 @@ public class GlobalCorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("/**")
+            .allowedOrigins("http://localhost:8081", "https://skillmesh-backend.onrender.com", "*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
